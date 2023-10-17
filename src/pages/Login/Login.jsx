29c5bd6dc141cc68fef80476968f91ef9fa5ec6e
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2023-10-17 14:30:18 +0200

    Création de la page Profile

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,7 +8,7 @@ function Login () {
     return (
         <div className='signin-page'>
             < Header />
-            <main id='bg-dark'>
+            <main className='bg-dark'>
                 < Form />
             </main>
             < Footer />
--- src/pages/User/User.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function User () {
-    return (
-        <h2>User Profile</h2>
-    )
-}
-
-export default User
