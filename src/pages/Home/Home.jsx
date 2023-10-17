7a384c068ca5475b74c8c4c67368b7adb731ab6d
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2023-10-17 16:05:52 +0200

    Amélioration des pages Home et Profile

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,19 @@ import Header from '../../components/Header.jsx';
 import Banner from '../../components/Banner.jsx';
 import Item from '../../components/Item.jsx';
 import Footer from '../../components/Footer.jsx';
+import FeaturesItemData from '../../data/FeaturesItemData.json';
 import iconChat from '../../assets/img/icon-chat.png';
 import iconMoney from '../../assets/img/icon-money.png';
 import iconSecurity from '../../assets/img/icon-security.png';
 import '../../sass/pages/_Home.scss';
 
 function Home () {
+    const imageData = {
+        "icon-chat.png": iconChat,
+        "icon-money.png": iconMoney,
+        "icon-security.png": iconSecurity
+    }
+
     return (
         <div className='homepage'>
             <Header />
@@ -16,24 +23,15 @@ function Home () {
                 <Banner />
                 <section className="features">
                     <h2 className='sr-only'>Features</h2>
-                    <Item 
-                        image={iconChat}
-                        descriptionImage="Chat Icon"
-                        title="You are our #1 priority"
-                        description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes. "
-                    />
-                    <Item 
-                        image={iconMoney}
-                        descriptionImage="Money Icon"
-                        title="More savings means higher rates"
-                        description="The more you save with us, the higher your interest rate will be! "  
-                    />                
-                    <Item 
-                        image={iconSecurity}
-                        descriptionImage="Security Icon"
-                        title="Security you can trust"
-                        description="We use top of the line encryption to make sure your data and money is always safe. "
-                    />
+                    {FeaturesItemData.map((data) => (
+                        < Item 
+                            key={data.id}
+                            image={imageData[data.image]}
+                            descriptionImage={data.descriptionImage}
+                            title={data.title}
+                            description={data.description}
+                        />
+                    ))}
                 </section>
             </main>
             < Footer />
